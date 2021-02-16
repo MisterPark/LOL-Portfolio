951a253a5b01021680296f98251eb209599ccdd5
MisterPark <40124101+MisterPark@users.noreply.github.com>
2021-02-16 10:19:45 +0900

    빌드오류 수정

---
+++ D3DFramework/D3DFramework/Event.cpp
@@ -0,0 +1,20 @@
+﻿#include "stdafx.h"
+#include "Event.h"
+
+Event& Event::operator+=(const EventHandler& handler)
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+Event& Event::operator-=(const EventHandler& handler)
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+void Event::AddHandler(const EventHandler& handler)
+{
+}
+
+void Event::RemoveHandler(const EventHandler& handler)
+{
+}
---
+++ D3DFramework/D3DFramework/Event.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class EventHandler
+{
+
+protected:
+	void(*Func)() = nullptr;
+};
+
+class Event
+{
+public:
+	
+	Event& operator+=(const EventHandler& handler);
+	Event& operator-=(const EventHandler& handler);
+
+	void AddHandler(const EventHandler& handler);
+	void RemoveHandler(const EventHandler& handler);
+};
+
