960279e66730776a2e82ef8ca06b1799590d5202
Damezuma <qweads12@gmail.com>
2021-03-16 23:18:37 +0900

    그냥 Effect를 Clone하도록 변경

--- D3DFramework/D3DFramework/ForwardRenderer.cpp
+++ D3DFramework/D3DFramework/ForwardRenderer.cpp
@@ -4,44 +4,42 @@
 Engine::ForwardRenderer::ForwardRenderer(GameObject* const owner, std::wstring const& path):
 	Renderer(owner, RendererType::Forward)
 {
+	IDirect3DDevice9 * const device = RenderManager::GetDevice();
 	effect = RenderManager::LoadEffect(path.c_str());
+	effect->CloneEffect(device, &effect);
 	mesh = nullptr;
 	pass = 0;
 }
 
+Engine::ForwardRenderer::~ForwardRenderer()
+{
+	Safe_Release(&effect);
+}
+
 void Engine::ForwardRenderer::SetValue(const char* parameter, float value)
 {
-	
-	parameterTable[parameter] = make_pair(&typeid(float), Parameter{ value });
+	effect->SetFloat(parameter, value);
 }
 
 void Engine::ForwardRenderer::SetValue(const char* parameter, Vector3 const& value)
 {
-	Parameter param{};
-	memcpy(&param.vector4, &value, sizeof(Vector3));
-	parameterTable[parameter] = make_pair(&typeid(D3DXVECTOR4), param);
-
+	D3DXVECTOR4 copy{ value.x,value.y, value.z, 0.f };
+	effect->SetVector(parameter, &copy);
 }
 
 void Engine::ForwardRenderer::SetValue(const char* parameter, D3DXVECTOR4 const& value)
 {
-	Parameter param{};
-	memcpy(&param.vector4, &value, sizeof(D3DXVECTOR4));
-	parameterTable[parameter] = make_pair(&typeid(D3DXVECTOR4), param);
+	effect->SetVector(parameter, &value);
 }
 
 void Engine::ForwardRenderer::SetValue(const char* parameter, Matrix const& value)
 {
-	Parameter param{};
-	memcpy(&param.vector4, &value, sizeof(Matrix));
-	parameterTable[parameter] = make_pair(&typeid(Matrix), param);
+	effect->SetMatrix(parameter, &value);
 }
 
 void Engine::ForwardRenderer::SetValue(const char* parameter, IDirect3DTexture9* const value)
 {
-	Parameter param{};
-	param.texture = value;
-	parameterTable[parameter] = make_pair(&typeid(IDirect3DTexture9*), param);
+	effect->SetTexture(parameter, value);
 }
 
 void Engine::ForwardRenderer::SetDiffuseTextureParam(const char* const parameter)
@@ -61,28 +59,6 @@ void Engine::ForwardRenderer::SetMesh(Mesh* mesh)
 
 void Engine::ForwardRenderer::Render()
 {
-	for (auto const& pair: parameterTable)
-	{
-		std::string const& key = pair.first;
-		std::type_info const* type = pair.second.first;
-		Parameter const& param = pair.second.second;
-		if (*type == typeid(float))
-		{
-			effect->SetFloat(key.c_str(), param.f32);
-		}
-		else if (*type == typeid(D3DXVECTOR4))
-		{
-			effect->SetVector(key.c_str(), (D3DXVECTOR4 const*)param.vector4);
-		}
-		else if (*type == typeid(Matrix))
-		{
-			effect->SetMatrix(key.c_str(), (D3DXMATRIX const*)param.matrix);
-		}
-		else if (*type == typeid(IDirect3DTexture9*))
-		{
-			effect->SetTexture(key.c_str(), param.texture);
-		}
-	}
 	UINT passCount{};
 	int const subsetCount = this->mesh->GetSubsetCount();
 	Matrix mViewSpace = Camera::main->GetViewMatrix();
